a450bdbd4cd0a5f23135a9c27ba5dd0f7584da1a
CVHIVE <CVHIVE@users.noreply.github.com>
2026-02-19 03:54:20 +0000

    Update footer copyright year to 2026

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -40,7 +40,7 @@
         </div>
         
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2025 CV Hive. All rights reserved. Made with ❤️ in UAE</p>
+          <p>&copy; 2026 CV Hive. All rights reserved. Made with ❤️ in UAE</p>
         </div>
       </div>
     </footer>
